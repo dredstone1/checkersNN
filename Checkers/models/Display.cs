918a3151ab74db0ca63cc710dae05692e880ce9e
maayan <mayanpotu500@gmail.com>
2026-06-30 15:24:43 +0300

    adding board

--- Checkers/models/Display.cs
+++ Checkers/models/Display.cs
@@ -6,6 +6,11 @@ namespace Checkers.models;
 
 public class Display
 {
+    const int GRID_RES = 600;
+
+    const int GRID_SIZE = 8;
+    const int SQUARE_RES = GRID_RES/GRID_SIZE;
+
     private bool running = false;
     public bool IsRunning
     {
@@ -38,12 +43,38 @@ public class Display
         DrawBoard();
     }
 
-    void DrawBoard() { }
+    Color getSquareColor(int i)
+    {
+        if ((i + (i / GRID_SIZE)) % 2 == 0)
+        {
+            return Color.White;
+        }
+        else
+        {
+            return Color.Black;
+        }
+    }
+
+    void DrawBoard()
+    {
+        for (int i = 0; i < GRID_SIZE*GRID_SIZE; ++i)
+        {
+            DrawSquare(((i % GRID_SIZE) * SQUARE_RES, (i / GRID_SIZE) * SQUARE_RES), getSquareColor(i));
+        }
+    }
+
+    void DrawSquare(Vector2f pos, Color c)
+    {
+        RectangleShape rect = new RectangleShape(new Vector2f(SQUARE_RES, SQUARE_RES));
+        rect.Position = pos + (20, 20);
+        rect.FillColor = c;
+        window.Draw(rect);
+    }
 
     public void Update()
     {
         window.DispatchEvents();
-        window.Clear(Color.White);
+        window.Clear(Color.Yellow);
 
         Draw();
 
--- Checkers/obj/Debug/net10.0/Checkers.AssemblyInfo.cs
+++ Checkers/obj/Debug/net10.0/Checkers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Checkers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c77e6bca6cd2f72e8fd963cd4eafc73c8db1365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7286a54eac7195d19fd0e5ff362b35b5ee8156f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Checkers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Checkers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
