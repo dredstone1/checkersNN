177d1d0c8320ecc6652a87cd968e460105143806
slomg <74960133+slomg@users.noreply.github.com>
2026-07-01 11:34:10 +0300

    Add evaluator

---
+++ Engine/Evaluators/Evaluator.cs
@@ -0,0 +1,21 @@
+﻿namespace Engine.Evaluators;
+
+public static class Evaluator
+{
+    public static bool TryEvaluateTermination(BitBoard board, int depth, out int terminationEval)
+    {
+        if (board.WhitePieces == 0)
+        {
+            terminationEval = board.IsWhiteToMove ? -100_000 - depth : 100_000 + depth;
+            return true;
+        }
+        else if (board.BlackPieces == 0)
+        {
+            terminationEval = board.IsWhiteToMove ? 100_000 + depth : -100_000 - depth;
+            return true;
+        }
+
+        terminationEval = 0;
+        return false;
+    }
+}
