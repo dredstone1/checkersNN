a11e5dc46ae271de2f0cf0aed5548627867fc613
slomg <74960133+slomg@users.noreply.github.com>
2026-07-01 10:26:57 +0300

    Add material value

---
+++ Engine/MaterialValue.cs
@@ -0,0 +1,15 @@
+﻿using Engine.Models;
+
+namespace Engine;
+
+public static class MaterialValue
+{
+    public static int GetPieceValue(PieceType type) =>
+        type switch
+        {
+            PieceType.Man => 100,
+            PieceType.King => 300,
+
+            _ => 0,
+        };
+}
