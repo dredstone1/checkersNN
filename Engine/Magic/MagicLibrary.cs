a58793a1f8c1dd42be5b6bb24a958409044395ac
slomg <74960133+slomg@users.noreply.github.com>
2026-07-01 11:48:03 +0300

    Add magic

---
+++ Engine/Magic/MagicLibrary.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+using MessagePack;
+
+namespace Engine.Magic;
+
+public static class MagicLibrary
+{
+    public static MagicPieceTable KingTable { get; }
+
+    private static readonly string _magicBasePath = Path.Combine(
+        AppContext.BaseDirectory,
+        "MagicTables"
+    );
+
+    static MagicLibrary()
+    {
+        KingTable = LoadTable("KingMagic.msgpack");
+    }
+
+    private static MagicPieceTable LoadTable(string fileName)
+    {
+        byte[] bytes = File.ReadAllBytes(Path.Combine(_magicBasePath, fileName));
+        return MessagePackSerializer.Deserialize<MagicPieceTable>(bytes);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static UInt128 GetAttacks(MagicPieceTable table, int square, UInt128 occupancy)
+    {
+        UInt128 blockers = occupancy & table.Masks[square];
+        UInt128 index = (blockers * table.MagicNumbers[square]) >> table.Shifts[square];
+        return table.AttackTable[square][(int)index];
+    }
+}
---
+++ Engine/Magic/MagicPieceTable.cs
@@ -0,0 +1,19 @@
+﻿using MessagePack;
+
+namespace Engine.Magic;
+
+[MessagePackObject]
+public class MagicPieceTable
+{
+    [Key(0)]
+    public required UInt128[] Masks { get; set; }
+
+    [Key(1)]
+    public required UInt128[] MagicNumbers { get; set; }
+
+    [Key(2)]
+    public required int[] Shifts { get; set; }
+
+    [Key(3)]
+    public required UInt128[][] AttackTable { get; set; }
+}
